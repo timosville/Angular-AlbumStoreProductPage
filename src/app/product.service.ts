14866e5e45303fa29a6510469bfeb9abefe4839d
Timothy Koech <timosville@gmail.com>
2019-10-23 10:52:13 +0300

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,6 +9,6 @@ export class ProductService {
   private _albumUrl = '../assets/album.json';
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(response => response.json);
+    return this._http.get(this._albumUrl).map(response => response.json());
   }
 }
